e63a1aa6d5c94b452c40ff54b9866190350ca2b8
Sean Hobbs <seanphobbs@gmaill.com>
2016-02-18 19:45:28 -0500

    This is my first commit. I have created all of the methods from the uml diagram and have begun completing them.

---
+++ src/coeficentProblem/demo.java
@@ -0,0 +1,60 @@
+package coeficentProblem;
+import javax.swing.JOptionPane;
+public class demo {
+
+		private int setUp(){ 
+			int n = 0;
+			double [][] dCoefficients;
+			double [] dRightSide;
+			String x =JOptionPane.showInputDialog("How many unknowns are there?");
+			n=Integer.parseInt(x);
+			if(n==0){
+				n=3;
+			}else{
+				dCoefficients = new double[Math.abs(n)][Math.abs(n)];
+				dRightSide = new double[Math.abs(n)];
+				if(n<0){
+					n=-n;
+					generateEquations(n,dCoefficients,dRightSide);
+				}else{
+					for(int i=0;i<n;i++){
+						for(int j=0;j<n;j++){
+							String number = JOptionPane.showInputDialog("Enter a number");
+							int input = Integer.parseInt(number);
+							dCoefficients[i][j]=input;
+						}
+						String number = JOptionPane.showInputDialog("Enter a number");
+						int input = Integer.parseInt(number);
+						dRightSide[i]=input;
+					}
+				}
+			}
+			return n;
+		}
+		private void generateEquations(int n, double[][]coefficients, double[]rightSides){
+			for(int i=0;i<n;i++){
+				for(int j=0;j<n;j++){
+					int input=(int)(100*(Math.random()-.5));
+					coefficients[i][j]=input;
+				}
+				int input=(int)(100*(Math.random()-.5));
+				rightSides[i]=input;
+			}
+			
+		}
+		private static void print(String message, int n, double[][]coefficients, double[]rightSide){
+			
+		}
+		private void print(String message, int n, double[]solution){
+			
+		}
+		public double[]evaluate(int n, double[][]coefficients, double[]solution){
+			
+		}	
+		private void print(String message, int n,double[]origRS, double[]checkRS){
+			
+		}
+		public static void main(String[]args){
+			
+		}
+}
---
+++ src/coeficentProblem/equations.java
@@ -0,0 +1,25 @@
+package coeficentProblem;
+
+public class equations {
+	public static void decompistion(int n, double[][]coefficients, int[] oldRow){
+		
+	}
+	private static double[] scaling(int n, double[][]coefficients){
+		
+	}
+	private static void upper(int col, double[][]coefficients){
+		
+	}
+	private static int beginLower(int col, double[][]coefficients, double[]scalings){
+		
+	}
+	public static double[] substitute(int n, double[]lowerUpper, int[]oldRow, double[]rightSides){
+		
+	}
+	private static double[] forward(int n, double[][]lowerUpper, int[] oldRow, double[]rightSides){
+		
+	}
+	private static void backward(int n, double[][]lowerUpper, double[]rightSides){
+		
+	}
+}
