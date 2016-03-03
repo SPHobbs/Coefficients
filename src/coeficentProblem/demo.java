977f546210df0b60363bf0d703bebe185943aafe
Sean Hobbs <seanphobbs@gmaill.com>
2016-03-02 22:52:03 -0500

    I have finished all of the methods and am working on fixing some printing errors

--- src/coeficentProblem/demo.java
+++ src/coeficentProblem/demo.java
@@ -1,36 +1,42 @@
 package coeficentProblem;
 import javax.swing.JOptionPane;
 public class demo {
-
-		private int setUp(){ 
-			int n = 0;
-			double [][] dCoefficients;
-			double [] dRightSide;
-			String x =JOptionPane.showInputDialog("How many unknowns are there?");
+		
+		static int n = 0;
+		static double[][] dCoefficients = null;
+		static double[] dRightSides = null;
+		static double[] solution = null;
+		static String message = null;
+		static double[][] rightSide = null;
+		static double[] dRightSide = null;
+		
+		private int setUp(){
+			String x =JOptionPane.showInputDialog("How many equations and unknowns are there?");
 			n=Integer.parseInt(x);
 			if(n==0){
 				n=3;
 			}else{
 				dCoefficients = new double[Math.abs(n)][Math.abs(n)];
-				dRightSide = new double[Math.abs(n)];
+				dRightSides = new double[Math.abs(n)];
 				if(n<0){
 					n=-n;
-					generateEquations(n,dCoefficients,dRightSide);
+					generateEquations(n,dCoefficients,dRightSides);
 				}else{
 					for(int i=0;i<n;i++){
 						for(int j=0;j<n;j++){
-							String number = JOptionPane.showInputDialog("Enter a number");
+							String number = JOptionPane.showInputDialog("Enter "+j+" coefficent");
 							int input = Integer.parseInt(number);
 							dCoefficients[i][j]=input;
 						}
-						String number = JOptionPane.showInputDialog("Enter a number");
+						String number = JOptionPane.showInputDialog("Enter the right side value");
 						int input = Integer.parseInt(number);
-						dRightSide[i]=input;
+						dRightSides[i]=input;
 					}
 				}
 			}
 			return n;
 		}
+		
 		private void generateEquations(int n, double[][]coefficients, double[]rightSides){
 			for(int i=0;i<n;i++){
 				for(int j=0;j<n;j++){
@@ -42,25 +48,27 @@ public class demo {
 			}
 			
 		}
-		private static void print(String message, int n, double[][]coefficients, double[]rightSide){
+		
+		private static void print(String message, int n, double[][]coefficients, double[]rightSides){
 			System.out.print("The coefficients of the equation are: ");
 			for(int i=0;i<n;i++){
 				for(int j=0;j<n;j++){
-					System.out.print(coefficients[i][j]);
+					System.out.print(coefficients[i][j]+" ");
 				}
-			}
-			
-			System.out.print("The solutions to the equation are: ");
+			}			
+			System.out.print("\nThe solutions to the equation are: ");
 			for(int i=0;i<n;i++){
-				System.out.print(rightSide[i]);
+				System.out.print(rightSides[i]+ " ");
 			}
 		}
+		
 		private void print(String message, int n, double[]solution){
-			System.out.print("The solutions to the equation are: ");
+			System.out.print("\nThe solutions to the equation are: ");
 			for(int i=0;i<n;i++){
 				System.out.print(solution[i]);
 			}
 		}
+		
 		public double[]evaluate(int n, double[][]coefficients, double[]rightSides, double[]solution){
 			for(int i=0;i<n;i++){
 				for(int j=0;j<n;j++){
@@ -69,10 +77,40 @@ public class demo {
 			}
 			return rightSides;
 		}	
+		
 		private void print(String message, int n,double[]origRS, double[]checkRS){
+			System.out.println("Right side");
+			for(int i=0;i<n;i++){
+				System.out.println(origRS[i]);
+			}
 			
+			System.out.println("Check right side");
+			for(int i=0;i<n;i++){
+				System.out.println(checkRS[i]);
+			}
 		}
-		public static void main(String[]args){
+		
+		public static void main(String[]args){			
 			new demo().setUp();
+			new demo().print("Test", n, dCoefficients, dRightSides);
+			
+			double[][] coefficients = new double[n][n];
+			double[] rightSides=new double[n];
+			int [] oldRow=new int [n];
+			
+			for(int i=0;i<n;i++){
+				for(int j=0;j<n;j++){
+					coefficients[i][j]=dCoefficients[i][j];
+					}
+				rightSides[i]=dRightSides[i];
+			}
+			equations.decompistion(n,coefficients,oldRow);
+			equations.substitute(n, coefficients, oldRow, rightSides);
+			
+			
+			
+			new demo().print(message, n, solution);
+			new demo().evaluate(n, dCoefficients, dRightSides, solution);
+			new demo().print(message, n, rightSide, dRightSide);
 		}
 }
--- src/coeficentProblem/equations.java
+++ src/coeficentProblem/equations.java
@@ -3,12 +3,13 @@ package coeficentProblem;
 public class equations {
 	public static void decompistion(int n, double[][]coefficients, int[] oldRow){
 		double tempValue;
+		double scaling[] = scaling(n,coefficients);
 		for(int i=0;i<n;i++){
 			oldRow[i] = i;
 		}
 		for(int j=0;j<n;j++){
-			upper(j, coefficients);
-			int maxRow = beginLower(j, coefficients, scaling(n, coefficients));
+			upper(n,j, coefficients);
+			int maxRow = beginLower(n,j, coefficients, scaling);
 			if(maxRow!=j){
 				for(int k=0;k<n;k++){
 					tempValue=coefficients[maxRow][k];
@@ -18,32 +19,85 @@ public class equations {
 				tempValue=oldRow[maxRow];
 				oldRow[maxRow]=oldRow[j];
 				oldRow[j]=(int) tempValue;
-				scaling(n, maxRow)=scaling(n, j);
+				scaling[maxRow]=scaling[j];
 				
 			}
-		}
-		
-		System.out.print("The solutions to the equation are: ");
-		for(int i=0;i<n;i++){
-			System.out.print(rightSide[i]);
+			if(j!=(n-1)){
+				for(int i=(j+1);i<n;i++){
+					coefficients[i][j]/=coefficients[j][j];
+				}
+			}
 		}
 	}
 	private static double[] scaling(int n, double[][]coefficients){
-		
+		double[] scalings = new double[n];
+		for(int i = 0;i<n;i++){
+			double max=0;
+			for(int j = 0;j<n;j++){
+				if(Math.abs(coefficients[i][j])>max){
+					max=Math.abs(coefficients[i][j]);
+				}
+			}
+			if(max==0){
+				System.out.println("Error: All of the coefficients in a row are zero");
+			}
+			scalings[i]=1/max;
+		}
+		return scalings;
 	}
-	private static void upper(int col, double[][]coefficients){
-		
+	private static void upper(int n, int col, double[][]coefficients){
+		for(int i=0;i<n;i++){
+			double sum = coefficients[i][col];
+			for(int k=0;k<n;k++){
+				sum-=coefficients[i][k]*coefficients[k][col];
+			}
+			coefficients[i][col]=sum;
+		}
 	}
-	private static int beginLower(int col, double[][]coefficients, double[]scalings){
-		
+	private static int beginLower(int n,int col, double[][]coefficients, double[]scalings){
+		int rowMax=col;
+		double max = 0;
+		for(int i=col;i<n;i++){
+			double sum=coefficients[i][col];
+			for(int k=0;k<col;k++){
+				sum-=coefficients[i][k]*coefficients[k][col];
+			}
+			coefficients[i][col]=sum;
+			if(scalings[i]*Math.abs(sum)>=max){
+				max=scalings[i]*Math.abs(sum);
+				rowMax=(int) max;
+			}
+		}
+		return rowMax;
 	}
-	public static double[] substitute(int n, double[]lowerUpper, int[]oldRow, double[]rightSides){
-		
+	public static double[] substitute(int n, double[][]lowerUpper, int[]oldRow, double[]rightSides){
+		rightSides=forward(n,lowerUpper,oldRow,rightSides);
+		backward(n,lowerUpper,rightSides);
+		return rightSides;
 	}
 	private static double[] forward(int n, double[][]lowerUpper, int[] oldRow, double[]rightSides){
-		
+		int firstNonZeroRow=-1;
+		double[] solution=new double[n];
+		for(int i=0;i<n;i++){
+			double sum = rightSides[oldRow[i]];
+			if(firstNonZeroRow>-1){
+				for(int j=firstNonZeroRow;j<i;j++){
+					
+				}
+			}else if(sum!=0){
+				firstNonZeroRow=i;
+			}
+			solution[i]=sum;
+		}
+		return solution;
 	}
 	private static void backward(int n, double[][]lowerUpper, double[]rightSides){
-		
+		for(int i=n-1;i>=0;i++){
+			double sum = rightSides[i];
+			for(int j=i+1;j<i;j++){
+				sum-=lowerUpper[i][j]*rightSides[j];
+			}
+			rightSides[i]=sum/lowerUpper[i][i];
+		}
 	}
 }
